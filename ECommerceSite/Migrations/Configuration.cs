a03725cbe26384848443a3d8db657434cca637ff
Nick SC <machiavellin342@gmail.com>
2015-01-15 14:05:43 -0800

    Enabled Automatic Migrations

---
+++ ECommerceSite/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ECommerceSite.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ECommerceSite.Infrastructure.ECommerceSiteDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ECommerceSite.Infrastructure.ECommerceSiteDB";
+        }
+
+        protected override void Seed(ECommerceSite.Infrastructure.ECommerceSiteDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
